8063fbab1d6471953ac2664cb85ef3f16f1962e6
AlexMorenoDev <alx.moreno.lobato@gmail.com>
2024-04-10 13:16:59 +0200

    Added paths for deployment in 'download_media_and_data.py'

--- src/scripts/download_media_and_data.py
+++ src/scripts/download_media_and_data.py
@@ -6,9 +6,13 @@ import patoolib
 # When deploying the application, as the "init_model.py" takes so much time to finish, second option will be done
 def main():
     url = "https://drive.google.com/uc?id=" + os.environ["gdrive_file_id"]
-    download_output = "../../static/pokemon.rar"
+    download_output = "../static/pokemon.rar" # For deployment
+    #download_output = "../../static/pokemon.rar" # For localhost
     gdown.download(url, download_output, quiet=False)
-    patoolib.extract_archive(download_output, outdir="../../static/")
+
+    extract_output = "../static/" # For deployment
+    #extract_output = "../../static/" # For localhost
+    patoolib.extract_archive(download_output, outdir=extract_output)
     os.remove(download_output)
 
 
